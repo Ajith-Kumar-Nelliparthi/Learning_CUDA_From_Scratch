2cdd3f7d35a2a2fff12b88022741f17d3cf15ae6
Ajith Kumar <nelliparthi123@gmail.com>
2025-12-09 08:01:29 +0530

    solved relu in LeetGpu

---
+++ Day 19/relu.cu
@@ -0,0 +1,18 @@
+#include <cuda_runtime.h>
+
+__global__ void relu_kernel(const float* input, float* output, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i=idx; i<N; i+= stride){
+        output[i] = fmaxf(0.0f, input[i]);
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    relu_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
