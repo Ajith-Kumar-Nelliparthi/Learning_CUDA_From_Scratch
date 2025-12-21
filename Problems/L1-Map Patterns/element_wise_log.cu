cb96272cd6477af45d22864cfaca9b513fd8b127
Ajith Kumar <nelliparthi123@gmail.com>
2025-12-21 07:44:32 +0530

    fixed typo

--- Problems/L1-Map Patterns/element_wise_log.cu
+++ Problems/L1-Map Patterns/element_wise_log.cu
@@ -6,7 +6,7 @@ __global__ void warmup(){
     if (idx == 0) printf("Warmup complete!\n");
 }
 
-__global__ void absolute_value(const float *A, float *B, int N){
+__global__ void log(const float *A, float *B, int N){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
@@ -15,7 +15,7 @@ __global__ void absolute_value(const float *A, float *B, int N){
     }
 }
 
-__global__ void absolute_value1(const float *A, float *B, int N){
+__global__ void log1(const float *A, float *B, int N){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     int warpId = idx / 32;
@@ -25,7 +25,7 @@ __global__ void absolute_value1(const float *A, float *B, int N){
     }
 }
 
-__global__ void absolute_value2(const float *A, float *B, int N){
+__global__ void log2(const float *A, float *B, int N){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     int vecN = N /4;
