2ce6995de470e72adbc676cbd4d0d32bc70262ff
Ajith Kumar <nelliparthi123@gmail.com>
2025-12-21 07:30:22 +0530

    element wise square

---
+++ Problems/L1-Map Patterns/element_square.cu
@@ -0,0 +1,44 @@
+# include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void warmup(){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx == 0) printf("Warmup complete!\n");
+}
+
+__global__ void absolute_value(const float *A, float *B, int N){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i=idx; i<N; i+=stride){
+        B[i] = sqrtf(A[i] * A[i]);
+    }
+}
+
+__global__ void absolute_value1(const float *A, float *B, int N){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int warpId = idx / 32;
+    int laneId = idx % 32;
+    for (int i=32*warpId+laneId; i<N; i+=stride){
+        B[i] = sqrtf(A[i] * A[i]);
+    }
+}
+
+__global__ void absolute_value2(const float *A, float *B, int N){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int vecN = N /4;
+    for (int i=idx; i<vecN; i+=stride){
+        float4 a = ((float4*)A)[i];
+        float4 b;
+        b.x = sqrtf(a.x * a.x);
+        b.y = sqrtf(a.y * a.y);
+        b.z = sqrtf(a.z * a.z);
+        b.w = sqrtf(a.w * a.w);
+        ((float4*)B)[i] = b;
+    }
+    for (int i=vecN*4; i<N; i+=stride){
+        B[i] = sqrtf(A[i] * A[i]);
+    }
+}
