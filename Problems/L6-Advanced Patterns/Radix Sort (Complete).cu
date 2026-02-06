4bd7ffe613336d9e0d6543d499aae8990db4bd15
Ajith Kumar <nelliparthi123@gmail.com>
2026-02-06 05:48:58 +0530

    radix sort

--- Problems/L6-Advanced Patterns/Radix Sort (Complete).cu
+++ Problems/L6-Advanced Patterns/Radix Sort (Complete).cu
@@ -13,7 +13,7 @@
 #define M (N / THREADS_PER_BLOCK) // Number of blocks
 
 // phase-1: global histogram
-__global__ void phase1_histogram(int* input, int* global_hist, int n, int bit_shift){
+__global__ void histogram_kernel(int* input, int* global_hist, int n, int bit_shift){
     extern __shared__ int local_hist[];
     
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -38,7 +38,7 @@ __global__ void phase1_histogram(int* input, int* global_hist, int n, int bit_sh
 }
 
 // phase-2: exclusive scan on histogram
-__global__ void phase2_exclusive_scan(int* global_hist, int* scan_output, int total_elements){
+__global__ void scan_kernel(int* global_hist, int* scan_output, int total_elements){
     extern __shared__ int temp[];
 
     int tid = threadIdx.x;
@@ -60,7 +60,7 @@ __global__ void phase2_exclusive_scan(int* global_hist, int* scan_output, int to
 }
 
 // phase-3: global scatter
-__global__ void phase3_scatter(int* input, int* output, int* scan_offsets, int n, int bit_shift){
+__global__ void scatter_kernel(int* input, int* output, int* scan_offsets, int n, int bit_shift){
     __shared__ int local_offsets[BINS];
     __shared__ int shared_data[THREADS_PER_BLOCK];
 
@@ -90,4 +90,66 @@ __global__ void phase3_scatter(int* input, int* output, int* scan_offsets, int n
         int destination = local_offsets[digit] + local_rank;
         output[destination] = val;
     }
+}
+
+int main() {
+    size_t size = N * sizeof(int);
+    int *h_in = (int*)malloc(size);
+    for (int i = 0; i < N; i++) h_in[i] = rand() % 1000000;
+
+    int *d_in, *d_out, *d_hist, *d_scan;
+    cudaMalloc(&d_in, size);
+    cudaMalloc(&d_out, size);
+    cudaMalloc(&d_hist, M * BINS * sizeof(int));
+    cudaMalloc(&d_scan, M * BINS * sizeof(int));
+
+    cudaMemcpy(d_in, h_in, size, cudaMemcpyHostToDevice);
+
+    // Timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start); cudaEventCreate(&stop);
+    cudaEventRecord(start);
+
+    // RADIX SORT LOOP (8 passes for 32 bits, 4 bits at a time) ---
+    for (int bit = 0; bit < 32; bit += BITS) {
+        histogram_kernel<<<M, THREADS_PER_BLOCK>>>(d_in, d_hist, N, bit);
+        
+        // Scan the entire M * BINS histogram table
+        // We use 1 block, assuming M*BINS <= 1024
+        scan_kernel<<<1, 1024>>>(d_hist, d_scan, M * BINS);
+        
+        scatter_kernel<<<M, THREADS_PER_BLOCK>>>(d_in, d_out, d_scan, N, bit);
+
+        // Ping-pong pointers
+        int* temp = d_in;
+        d_in = d_out;
+        d_out = temp;
+    }
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float ms = 0;
+    cudaEventElapsedTime(&ms, start, stop);
+
+    // THRUST COMPARISON
+    thrust::device_vector<int> d_thrust(h_in, h_in + N);
+    clock_t t_start = clock();
+    thrust::sort(d_thrust.begin(), d_thrust.end());
+    clock_t t_end = clock();
+    float thrust_ms = (float)(t_end - t_start) * 1000.0 / CLOCKS_PER_SEC;
+
+    // Verify
+    cudaMemcpy(h_in, d_in, size, cudaMemcpyDeviceToHost);
+    bool success = true;
+    for (int i = 0; i < N - 1; i++) {
+        if (h_in[i] > h_in[i+1]) { success = false; break; }
+    }
+
+    printf("Sort %s\n", success ? "SUCCESSFUL" : "FAILED");
+    printf("Custom Radix Sort Time: %f ms\n", ms);
+    printf("Thrust Sort Time:       %f ms\n", thrust_ms);
+
+    cudaFree(d_in); cudaFree(d_out); cudaFree(d_hist); cudaFree(d_scan);
+    free(h_in);
+    return 0;
 }
