77259b570a2d977f1a2fff47962a56f9b50024c0
Ajith Kumar <nelliparthi123@gmail.com>
2026-07-04 11:58:21 +0530

    feat: torch binding added

--- LeetCUDA/kernels/dot_product'/dot_product.cu
+++ LeetCUDA/kernels/dot_product'/dot_product.cu
@@ -167,7 +167,7 @@ __global__ void dot_product_f16x2_f32_kernel(half *a, half *b, float *y, int N) 
 
 // fp 16 x 8
 template <const int NUM_THREADS = 256 / 8>
-__global__ void dot_product_f16x8_f32_kernel(half *a, half *b, float *y, int N) {
+__global__ void dot_product_f16x8_pack_f32_kernel(half *a, half *b, float *y, int N) {
     int tid = threadIdx.x;
     int idx = (blockIdx.x * blockDim.x + tid) * 8;
     constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
@@ -200,4 +200,109 @@ __global__ void dot_product_f16x8_f32_kernel(half *a, half *b, float *y, int N) 
         prod = warp_reduce_sum_f32<NUM_WARPS>(prod);
     if (tid == 0)
         atomicAdd(y, prod);
+}
+
+#define STRINGFY(str) #str
+#define TORCH_BINDING_COMMON_EXTENSION(func)                                   \
+  m.def(STRINGFY(func), &func, STRINGFY(func));
+
+#define CHECK_TORCH_TENSOR_DTYPE(T, th_type)                                   \
+  if (((T).options().dtype() != (th_type))) {                                  \
+    std::cout << "Tensor Info:" << (T).options() << std::endl;                 \
+    throw std::runtime_error("values must be " #th_type);                      \
+  }
+
+#define LANUCH_DOT_PROD_KERNEL(NT, packed_type, acc_type, element_type)        \
+  dot_prod_##packed_type##_##acc_type##_kernel<(NT)>                           \
+      <<<grid, block>>>(reinterpret_cast<element_type *>(a.data_ptr()),        \
+                        reinterpret_cast<element_type *>(b.data_ptr()),        \
+                        prod.data_ptr<float>(), N);
+
+#define DISPATCH_DOT_PROD_KERNEL(K, packed_type, acc_type, element_type,       \
+                                 n_elements)                                   \
+  const int NT = (K) / (n_elements);                                           \
+  dim3 block(NT);                                                              \
+  dim3 grid((S));                                                              \
+  switch (NT) {                                                                \
+  case 32:                                                                     \
+    LANUCH_DOT_PROD_KERNEL(32, packed_type, acc_type, element_type)            \
+    break;                                                                     \
+  case 64:                                                                     \
+    LANUCH_DOT_PROD_KERNEL(64, packed_type, acc_type, element_type)            \
+    break;                                                                     \
+  case 128:                                                                    \
+    LANUCH_DOT_PROD_KERNEL(128, packed_type, acc_type, element_type)           \
+    break;                                                                     \
+  case 256:                                                                    \
+    LANUCH_DOT_PROD_KERNEL(256, packed_type, acc_type, element_type)           \
+    break;                                                                     \
+  case 512:                                                                    \
+    LANUCH_DOT_PROD_KERNEL(512, packed_type, acc_type, element_type)           \
+    break;                                                                     \
+  case 1024:                                                                   \
+    LANUCH_DOT_PROD_KERNEL(1024, packed_type, acc_type, element_type)          \
+    break;                                                                     \
+  default:                                                                     \
+    throw std::runtime_error(                                                  \
+        "only support (K)/(n_elements): 32/64/128/256/512/1024");              \
+    break;                                                                     \
+  }
+
+#define TORCH_BINDING_DOT_PROD(packed_type, acc_type, th_type, element_type,   \
+                               n_elements)                                     \
+  torch::Tensor dot_prod_##packed_type##_##acc_type(torch::Tensor a,           \
+                                                    torch::Tensor b) {         \
+    CHECK_TORCH_TENSOR_DTYPE(a, (th_type))                                     \
+    CHECK_TORCH_TENSOR_DTYPE(b, (th_type))                                     \
+    auto options =                                                             \
+        torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA, 0); \
+    auto prod = torch::zeros({1}, options);                                    \
+    const int ndim = a.dim();                                                  \
+    if (ndim != 2) {                                                           \
+      int N = 1;                                                               \
+      for (int i = 0; i < ndim; ++i) {                                         \
+        N *= a.size(i);                                                        \
+      }                                                                        \
+      dim3 block(256);                                                         \
+      dim3 grid(((N + 256 - 1) / 256) / (n_elements));                         \
+      dot_prod_##packed_type##_##acc_type##_kernel<256>                        \
+          <<<grid, block>>>(reinterpret_cast<element_type *>(a.data_ptr()),    \
+                            reinterpret_cast<element_type *>(b.data_ptr()),    \
+                            prod.data_ptr<float>(), N);                        \
+    } else {                                                                   \
+      const int S = a.size(0);                                                 \
+      const int K = a.size(1);                                                 \
+      const int N = S * K;                                                     \
+      if ((K / (n_elements)) <= 1024) {                                        \
+        DISPATCH_DOT_PROD_KERNEL(K, packed_type, acc_type, element_type,       \
+                                 n_elements)                                   \
+      } else {                                                                 \
+        int N = 1;                                                             \
+        for (int i = 0; i < ndim; ++i) {                                       \
+          N *= a.size(i);                                                      \
+        }                                                                      \
+        dim3 block(256);                                                       \
+        dim3 grid(((N + 256 - 1) / 256) / (n_elements));                       \
+        dot_prod_##packed_type##_##acc_type##_kernel<256>                      \
+            <<<grid, block>>>(reinterpret_cast<element_type *>(a.data_ptr()),  \
+                              reinterpret_cast<element_type *>(b.data_ptr()),  \
+                              prod.data_ptr<float>(), N);                      \
+      }                                                                        \
+    }                                                                          \
+    return prod;                                                               \
+  }
+
+// packed_type, acc_type, th_type, element_type, n_elements_per_pack
+TORCH_BINDING_DOT_PROD(f32, f32, torch::kFloat32, float, 1)
+TORCH_BINDING_DOT_PROD(f32x4, f32, torch::kFloat32, float, 4)
+TORCH_BINDING_DOT_PROD(f16, f32, torch::kHalf, half, 1)
+TORCH_BINDING_DOT_PROD(f16x2, f32, torch::kHalf, half, 2)
+TORCH_BINDING_DOT_PROD(f16x8_pack, f32, torch::kHalf, half, 8)
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  TORCH_BINDING_COMMON_EXTENSION(dot_prod_f32_f32)
+  TORCH_BINDING_COMMON_EXTENSION(dot_prod_f32x4_f32)
+  TORCH_BINDING_COMMON_EXTENSION(dot_prod_f16_f32)
+  TORCH_BINDING_COMMON_EXTENSION(dot_prod_f16x2_f32)
+  TORCH_BINDING_COMMON_EXTENSION(dot_prod_f16x8_pack_f32)
 }
