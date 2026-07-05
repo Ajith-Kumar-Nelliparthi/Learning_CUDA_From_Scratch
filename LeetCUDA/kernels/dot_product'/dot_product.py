9cb9ece707bb85fff5b04fdb0ceba793570caada
Ajith Kumar <nelliparthi123@gmail.com>
2026-07-05 17:59:14 +0530

    feat: dot product python kernel added

---
+++ LeetCUDA/kernels/dot_product'/dot_product.py
@@ -0,0 +1,86 @@
+import time
+from typing import Optional
+import torch
+from torch.utils.cpp_extenstion import load
+
+torch.set_grad_enabled(False)
+
+# load the cuda kernel as a python module
+lib = load(
+    name="relu_lib",
+    sources=["relu.cu"],
+    extra_cuda_cflags=[
+        "-O3",
+        "-U__CUDA_NO_HALF_OPERATORS__",
+        "-U__CUDA_NO_HALF_CONVERSIONS__",
+        "-U__CUDA_NO_HALF2_OPERATORS__",
+        "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+        "--expt-relaxed-constexpr",
+        "--expt-extended-lambda",
+        "--use_fast_math",
+    ],
+    extra_cflags=["-std=c++17"],
+)
+
+def run_benchmark(
+    perf_func: callable,
+    x: torch.Tensor,
+    tag: str,
+    out: Optional[torch.Tensor] = None,
+    warmup: int = 10,
+    iters: int = 1000,
+    show_all: bool = False,
+):
+    if out is not None:
+        out.fill_(0)
+    # warmup
+    if out is not None:
+        for i in range(warmup):
+            perf_func(x, out)
+    else:
+        for i in range(warmup):
+            _ = perf_func(x)
+    torch.cuda.synchronize()
+
+    start = time.time()
+    # iters
+    if out is not None:
+        for i in range(iters):
+            perf_func(x, out)
+    else:
+        for i in range(iters):
+            out = perf_func(x)
+    torch.cuda.synchronize()
+    end = time.time()
+    total_time = (end - start) * 1000  # ms
+    mean_time = total_time / iters
+    out_info = f"out_{tag}"
+    out_val = out.flatten().detach().cpu().numpy().tolist()[:2]
+    out_val = [round(v, 8) for v in out_val]
+    out_val = [f"{v:<12}" for v in out_val]
+    print(f"{out_info:>18}: {out_val}, time:{mean_time:.8f}ms")
+    if show_all:
+        print(out)
+    return out, mean_time
+
+Ss = [1024, 2048, 4096]
+Ks = [1024, 2048, 4096]
+SKs = [(S, K) for S in Ss for K in Ks]
+
+for S, K in SKs:
+    print("-" * 80)
+    print(" " * 25 + f"S={S}, K={K}")
+    a = torch.randn((S * K)).cuda().float()
+    b = torch.randn((S * K)).cuda().float()
+    run_benchmark(lib.dot_prod_f32_f32, a, b, "f32f32")
+    run_benchmark(lib.dot_prod_f32x4_f32, a, b, "f32x4f32")
+    run_benchmark(torch.dot, a, b, "f32f32_th")
+
+    print("-" * 80)
+    a_f16 = a.half()
+    b_f16 = b.half()
+    run_benchmark(lib.dot_prod_f16_f32, a_f16, b_f16, "f16f32")
+    run_benchmark(lib.dot_prod_f16x2_f32, a_f16, b_f16, "f16x2f32")
+    run_benchmark(lib.dot_prod_f16x8_pack_f32, a_f16, b_f16, "f16x8packf32")
+    run_benchmark(torch.dot, a_f16, b_f16, "f16f16_th")
+    print("-" * 80)
