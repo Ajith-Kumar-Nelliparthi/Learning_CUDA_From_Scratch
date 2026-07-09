6b126c1e2852bec5a161a052d6272f3f3cde4a8f
Ajith Kumar <nelliparthi123@gmail.com>
2026-07-09 10:57:06 +0530

    feat: safe softmax 8 pack kernel added

--- LeetCUDA/kernels/softmax/softmax.cu
+++ LeetCUDA/kernels/softmax/softmax.cu
@@ -15,6 +15,7 @@
 #define INT4(value) (reinterpret_cast<int4 *>(&(value))[0])
 #define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
 #define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
+#define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
 #define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
 
 // FP 32
@@ -205,5 +206,77 @@ __global__ void safe_softmax_f16_f32_per_token_kernel(half *x, half *y, int N) {
 
     if (idx < N) {
         y[idx] = __float2half_rn(exp_sum / max_sum);
+    }
+}
+
+template <const int NUM_THREADS = 256>
+__global__ void safe_softmax_f16x2_f32_per_token_kernel(half *x, half *y, int N) {
+    const int tid = threadIdx.x;
+    const int idx = 2 * (blockIdx.x * blockDim.x + tid);
+
+    float reg_x = __half22float2(HALF2(x[idx]));
+    float max_val = -FLT_MAX;
+    max_val = ((idx + 0) < N) ? fmaxf(reg_x.x, max_val) : -FLT_MAX;
+    max_val = ((idx + 1) < N) ? fmaxf(reg_x.y, max_val) : -FLT_MAX;
+    max_val = block_reduce_max_f32<NUM_THREADS>(max_val);
+    
+    float2 reg_exp;
+    reg_exp.x = ((idx + 0) < N) ? expf(reg_x.x - max_val) : 0.0f;
+    reg_exp.y = ((idx + 1) < N) ? expf(reg_x.y - max_val) : 0.0f;
+    
+    float exp_val = reg_exp.x + reg_exp.y;
+    float exp_sum = block_reduce_sum_f32<NUM_THREADS>(exp_val);
+
+    float2 reg_y;
+    reg_y.x = reg_exp.x / exp_sum;
+    reg_y.y = reg_exp.y / exp_sum;
+
+    if ((idx + 1) < N) {
+        HALF2(y[idx]) = __float22half2_rn(reg_y);
+    }
+}
+
+template <const int NUM_THREADS = 256>
+__global__ void safe_softmax_f16x8_pack_f32_per_token_kernel(half *x, half *y, int N) {
+    const int tid = threadIdx.x;
+    const int idx = 8 * (blockIdx.x * blockDim.x + tid);
+    // temprorary registers
+    half pack_x[8], pack_y[8]; // 8 * 16 bits = 128 bits
+    // reinterpret as float4 and load 8 bits at once
+    LDST128BITS(pack_x[0]) = LDST128BITS(x[idx]); // load 128 bits
+
+    float max_val = -FLT_MAX;
+#pragma unroll
+    for (int i = 0; i < 8; i++) {
+        max_val = fmaxf(__half2float(pack_x[i]), max_val);
+    }
+    max_val = block_reduce_max_f32<NUM_THREADS>(max_val);
+
+    float exp_sum = 0.0f;
+    float exp_vals[8] = {0.0f};
+#pragma unroll
+    for (int i = 0; i < 8; i++) {
+        float val = __half2float(pack_x[i]);
+        float exp_val = (((idx + i) < N) ? expf(val - max_val) : 0.0f);
+        exp_vals[i] = exp_val;
+        exp_sum += exp_val;
+    }
+    exp_sum = block_reduce_sum_f32<NUM_THREADS>(exp_sum);
+
+#pragma unroll
+    for (int i = 0; i < 8; i++) {
+        pack_y[i] = __float2half_rn(exp_vals[i] / exp_sum);
+    }
+    // reinterpret as float4 and store 128 bits at 1 memory issue.
+    if ((idx + 7) < N) {
+        LDST128BITS(y[idx]) = LDST128BITS(pack_y[0]);
+    }
+    else if (idx < N) {
+#pragma unroll
+        for (int i = 0; i < 8; ++i) {
+            if (idx + i < N) {
+                y[idx + i] = pack_y[i];
+            }
+        }
     }
 }
