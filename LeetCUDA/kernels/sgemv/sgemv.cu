f094da81896f2be4b331d9969738ad33999dd086
Ajith Kumar <nelliparthi123@gmail.com>
2026-07-22 11:00:59 +0530

    feat: pytorch binding for k32, f32x4 kernels added

--- LeetCUDA/kernels/sgemv/sgemv.cu
+++ LeetCUDA/kernels/sgemv/sgemv.cu
@@ -6,6 +6,9 @@
 #include <math.h>
 #include <algorithm>
 #include <vector>
+#include <torch/extension.h>
+#include <torch/types.h>
+
 
 #define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
 #define INT4(value) (reinterpret_cast<int4 *>(&(value))[0])
@@ -64,4 +67,74 @@ __global__ void sgemv_k128_f32x4_kernel(float *x, float *a, float *y, int M,int 
         sum = warp_reduce_sum<WARP_SIZE>(sum);
         if (lane == 0) y[m] = sum;
     } 
+}
+
+#define STRINGFY(str) #str
+#define TORCH_BINDING_COMMON_EXTENSION(func) \
+    m.def(STRINGFY(func), &func, STRINGFY(func));
+
+#define CHECK_TORCH_TENSOR_DTYPE(T, th_type)  \
+    if (((T).options().dtype() != (th_type))) { \
+        std::cout << "tensor info:" << (T).options() << std::endl; \
+        throw std::runtime_error("values must be " #th_type); \
+}
+
+#define CHECK_TORCH_TENSOR_SHAPE(T, S0, S1)                                    \
+  if (((T).size(0) != (S0)) || ((T).size(1) != (S1))) {                        \
+    throw std::runtime_error("Tensor size mismatch!");                         \
+  }
+
+#define ASSERT_K_IS_MULTIBLE_OF(V)                                             \
+  if (K % (V) != 0) {                                                          \
+    throw std::runtime_error("K must be multiple of " #V);                     \
+  }
+
+#define ASSERT_K_IS_EQUAL_OF(V)                                                \
+  if (K != (V)) {                                                              \
+    throw std::runtime_error("K must be " #V);                                 \
+}
+
+void sgemv_k32_f32(torch::Tensor a, torch::Tensor x, torch::Tensor y) {
+  CHECK_TORCH_TENSOR_DTYPE(a, torch::kFloat32)
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kFloat32)
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kFloat32)
+  const int M = a.size(0);
+  const int K = a.size(1);
+  CHECK_TORCH_TENSOR_SHAPE(a, M, K)
+  CHECK_TORCH_TENSOR_SHAPE(x, K, 1)
+  CHECK_TORCH_TENSOR_SHAPE(y, M, 1)
+  ASSERT_K_IS_MULTIBLE_OF(32)
+
+  dim3 block(32, 4);
+  dim3 grid((M + 4 - 1) / 4);
+
+  sgemv_k32_f32_kernel<<<grid, block>>>(reinterpret_cast<float *>(a.data_ptr()),
+                                        reinterpret_cast<float *>(x.data_ptr()),
+                                        reinterpret_cast<float *>(y.data_ptr()),
+                                        M, K);
+}
+
+void sgemv_k128_f32x4(torch::Tensor a, torch::Tensor x, torch::Tensor y) {
+  CHECK_TORCH_TENSOR_DTYPE(a, torch::kFloat32)
+  CHECK_TORCH_TENSOR_DTYPE(x, torch::kFloat32)
+  CHECK_TORCH_TENSOR_DTYPE(y, torch::kFloat32)
+  const int M = a.size(0);
+  const int K = a.size(1);
+  CHECK_TORCH_TENSOR_SHAPE(a, M, K)
+  CHECK_TORCH_TENSOR_SHAPE(x, K, 1)
+  CHECK_TORCH_TENSOR_SHAPE(y, M, 1)
+  ASSERT_K_IS_MULTIBLE_OF(128)
+
+  dim3 block(32, 4);
+  dim3 grid((M + 4 - 1) / 4);
+
+  sgemv_k128_f32x4_kernel<<<grid, block>>>(
+      reinterpret_cast<float *>(a.data_ptr()),
+      reinterpret_cast<float *>(x.data_ptr()),
+      reinterpret_cast<float *>(y.data_ptr()), M, K);
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  TORCH_BINDING_COMMON_EXTENSION(sgemv_k32_f32)
+  TORCH_BINDING_COMMON_EXTENSION(sgemv_k128_f32x4)
 }
