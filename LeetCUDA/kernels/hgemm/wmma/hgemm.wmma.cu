5866df28315924b2d5c6154559dac467e984a4f1
Ajith Kumar <nelliparthi123@gmail.com>
2026-08-29 11:01:33 +0530

    feat: add wmma_mma kernel, smem loads

--- LeetCUDA/kernels/hgemm/wmma/hgemm.wmma.cu
+++ LeetCUDA/kernels/hgemm/wmma/hgemm.wmma.cu
@@ -69,3 +69,40 @@ int div_ceil(int a, int b) { return (a % b != 0) ? (a / b + 1) : (a / b); }
     // store results
     wmma::store_matrix_sync(C + load_gmem_a_m * N + load_gmem_b_n, C_frag, N, wmma::row_major)
  }
+
+ template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16, const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2>
+ __global__ void hgemm_wmma_m16n16k16_mma4x2_kernel(half *A, half *B, half *C, int M, int N, int K) {
+    const int bx = blockIdx.x;
+    const int by = blockIdx.y; // block id's in x dir
+    const int NUM_K_TILES = div_ceil(K, WMMA_K);
+    const int BM = WMMA_M * WMMA_TILE_M; // 16 x 4 = 64
+    const int BN = WMMA_N * WMMA_TILE_N; // 16 x 2 = 32
+    __shared__ half s_a[BM][WMMA_K], s_b[WMMA_K][BN];     // 64x16x2bytes=2kb 16x32x2bytes=1kb
+
+    // 1D thread index
+    const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+    const int warp_id = tid / WARP_SIZE;    // 0 to 7
+    const int lane_id = tid % WARP_SIZE;   // 0 to 31
+    const int warp_m = warp_id / 2;     // 0,1,2,3
+    const int warp_n = warp_id % 2;     // 0,1
+
+    // shared memory index calculation
+    int load_smem_a_m = tid / 4;   //0-63
+    int load_smem_a_k = (tid % 4) * 4;  // 0,4,8,..
+    int load_smem_b_k = tid / 16;
+    int load_smem_b_n = (tid % 16) * 2;     // 0,2,4,,32
+    int load_gmem_a_m = by * BM + load_smem_a_m;    // global m
+    int load_gmem_b_n = bx * BN + load_smem_b_n;    // global n
+    if (load_gmem_a_m >= M || load_gmem_b_n >= N) return;
+
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> C_frag;
+    wmma::fill_fragment(C_frag, 0.0);
+
+#pragma unroll
+    for (int k=0; k<NUM_K_TILES; k++) {
+        int load_gmem_a_k = k * WMMA_K + load_smem_a_k;
+        int load_gmem_a_addr = load_gmem_a_m * K + load_gmem_a_k;
+        int load_gmem_b_k = k * WMMA_K + load_smem_b_k;
+        int load_gmem_b_addr = load_gmem_b_k * N + load_gmem_b_n;
+    }
+ }
