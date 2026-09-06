f8ebe081f6f1f0bbd9f42732c8b403093c70c2aa
Ajith Kumar <137274490+Ajith-Kumar-Nelliparthi@users.noreply.github.com>
2026-09-06 10:19:43 +0530

    feat: add hgemm 256x128 wmma prefill logic (#23)

--- LeetCUDA/kernels/hgemm/wmma/hgemm_wmma_stage.cu
+++ LeetCUDA/kernels/hgemm/wmma/hgemm_wmma_stage.cu
@@ -561,3 +561,77 @@ __global__ void __launch_bounds__(512)
         }
     }
 }
+
+
+// 256 x 128
+template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16,
+            const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2,
+            const int WARP_TILE_M = 4, const int WARP_TILE_N = 4,
+            const int WARP_TILE_K = 1, const int A_PAD = 0, const int B_PAD + 0,
+            const int K_STAGE = 2, bool BLOCK_SWIZZLE = false>
+__global__ void __launch_bounds__(256) 
+    hgemm_wmma_m16n16k16_mma4x2_warp4x4_stages_dsmem_kernel(half *A, half *B, half *C, int M, int N, int K) {
+    const int bx = ((int)BLOCK_SWIZZLE) * blockIdx.z * gridDim.x + blockIdx.x;
+    const int by = blockIdx.y;
+    const int NUM_K_TILES = div_ceil(K, WMMA_K * WARP_TILE_K);
+    constexpr int BM = WMMA_M * WMMA_TILE_M * WARP_TILE_M;  //16x4x4 = 256
+    constexpr int BN = WMMA_N * WMMA_TILE_N * WARP_TILE_N;  //16x2x4 = 128
+    constexpr int BK = WMMA_K * WARP_TILE_K;
+
+    extern __shared__ half smem[];
+    half *s_a = smem;
+    half *s_b = smem + K_STAGE * BM * (BK + A_PAD);
+    constexpr int s_a_stage_offset = BM * (BK + A_PAD);
+    constexpr int s_b_stage_offset = BK * (BN + B_PAD);
+
+    // thread and warp index
+    const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+    const int warp_id = tid / WARP_SIZE;
+    const int lane_id = tid % WARP_SIZE;
+    const int warp_m = warp_id / 2;
+    const int warp_n = warp_id % 2;
+
+    int load_smem_a_m = tid;
+    int load_smem_a_k = 0;  // 1 thread reads 16 half elements
+    int load_smem_b_k = tid / 16;
+    int load_smem_b_n = (tid % 16) * 8;
+    int load_gmem_a_m = by * BM + load_smem_a_m;
+    int load_gmem_b_n = bx * BN + load_smem_b_n;
+    if (load_gmem_a_m >= M || load_gmem_b_n >= N) return;
+
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half>C_frag[WARP_TILE_M][WARP_TILE_N];
+#pragma unroll
+    for (int i=0; i<WARP_TILE_M; i++) {
+#pragma unroll
+        for (int j=0; j<WARP_TILE_N; j++) {
+            wmma::fill_fragment(C_frag[i][j], 0.0);
+        }
+    }
+
+    uint32_t smem_a_base_ptr = __cvta_generic_to_shared(s_a);
+    uint32_t smem_b_base_ptr = __cvta_generic_to_shared(s_b);
+
+    // prefill
+#pragma unroll
+    for (int k=0; k < (K_STAGE - 1); k++) {
+        int load_gmem_a_k = k * (WMMA_K * WARP_TILE_K) + load_smem_a_k;
+        int load_gmem_a_addr = load_gmem_a_m * K + load_gmem_a_k;
+        int load_gmem_b_k = k * (WMMA_K * WARP_TILE_K) + load_smem_b_k;
+        int load_gmem_b_addr = load_gmem_b_k * N + load_gmem_b_n;
+
+        uint32_t load_smem_a_ptr = 
+            (smem_a_base_ptr + (k * s_a_stage_offset + load_smem_a_m * (BK + A_PAD) + load_smem_a_k) * sizeof(half));
+        uint32_t load_smem_b_ptr = 
+            (smem_b_base_ptr + (k * s_b_stage_offset + load_smem_b_k * (BN + B_PAD) + load_smem_b_n) * sizeof(half));
+        
+        CP_ASYNC_CG(load_smem_a_ptr, &A[load_gmem_a_addr], 16);
+        CP_ASYNC_CA(load_smem_a_ptr + 16, &A[load_gmem_a_addr + 8], 16);
+        CP_ASYNC_CG(load_smem_b_ptr, &B[load_gmem_b_addr], 16);
+
+        CP_ASYNC_COMMIT_GROUP();
+    }
+    CP_ASYNC_WAIT_GROUP(K_STAGE - 2);
+    __syncthreads();
+
+    // main loop
+}
